5e9d26b59dce741a3c01686f69e6efb4caa4074d
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-03-20 13:47:25 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let img;
+let e;
 function setup() {
   createCanvas(400, 400);
+  img = loadImage("towerGood.png")
+  e = new Enemy(0,159,20,20)
+
 }
 
 function draw() {
   background(220);
+  img.resize(400,400)
+  image(img,0,0)
+  e.display()
+  
 }
