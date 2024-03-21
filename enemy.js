50b3b4f929b8e4c6646b9ef84fd49b7a7be5b7cb
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-03-21 12:57:25 -0500

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,14 +1,32 @@
-function Enemy(x,y,w,h){
+function Enemy(x,y,w,h,s){
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
+  this.section = s
+  this.speed = 1
   
   this.display = function(){
     rect(this.x, this.y, this.w, this.h)
   }
   
   this.followPath = function(){
-    this.x += 5
+    if(this.section == 1){
+      this.x += this.speed
+      if(this.x >188){
+        this.section += 1
+      }
+    }else if (this.section == 2){
+      this.y -= this.speed
+      if(this.y<68){
+        this.section += 1
+      }
+    }else if (this.section == 3){
+      this.x -= this.speed
+      if(this.x<120){
+        this.section += 1
+      }
+    }
+    
   }
 }
