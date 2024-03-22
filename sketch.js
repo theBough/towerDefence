824cd5ed2dcea61c0552fd8260bbe94d9f947322
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-03-22 12:40:39 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,27 @@
 let img;
 let e;
+let c;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 400);
   img = loadImage("towerGood.png")
   e = new Enemy(0,159,20,20,1)
-
+  c = new Cannon(410,50,30,30,"cannon.png")
 }
 
 function draw() {
   background(220);
   img.resize(400,400)
   image(img,0,0)
-  e.display()
-  e.followPath()
+   enemyStuff();
+  cannonStuff()
   
+}
+function cannonStuff(){
+     c.display() 
+    c.grab()
+}
+
+function enemyStuff(){
+   e.display()
+  e.followPath()
 }
