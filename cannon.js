5787efd88d18c188ff079ea2644223ae118327e1
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-03-22 12:39:33 -0500

    Update cannon.js

--- cannon.js
+++ cannon.js
@@ -9,4 +9,12 @@ function Cannon(x,y,w,h,img){
     this.img.resize(this.w, this.h);
     image(this.img, this.x, this.y)
   }
+  this.grab = function(){
+    if(mouseX > this.x &&
+       mouseX < this.x + this.w &&
+       mouseY > this.y &&
+       mouseY < this.y + this.h){
+      cursor("grab")
+    }
+  }
 }
