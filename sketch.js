8bf5bf7268a5b8b2ced0a0820424d28687e10350
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-01 12:54:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,58 @@
 let img;
 let e;
 let c;
+let x;
+let revenue;
 function setup() {
   createCanvas(500, 400);
-  img = loadImage("towerGood.png")
-  e = new Enemy(0,159,20,20,1)
-  c = new Cannon(410,50,30,30,"cannon.png")
+  img = loadImage("towerGood.png");
+  e = new Enemy(0, 159, 20, 20, 1);
+  c = new Cannon(410, 50, 30, 30, "cannon.png");
+  revenue = 1;
+  
 }
 
 function draw() {
   background(220);
-  img.resize(400,400)
-  image(img,0,0)
-   enemyStuff();
-  cannonStuff()
+  img.resize(400, 400);
+  image(img, 0, 0);
+  enemyStuff();
+  cannonStuff();
+  
+  /*
+  push()
+  angleMode(DEGREES)
+  translate(100,100)
+  rotate(x)
+  c.display()
+  rect(10,10,50,50)
+  pop()
+  x+=1
+  */
   
 }
-function mouseReleased(){
+function mouseReleased() {
   c.lock = false;
+  revenue -= 1
+  console.log(revenue )
 }
-function mouseDragged(){
-  if(c.lock){
-    c.x = mouseX-10
-    c.y = mouseY-10
+function mouseDragged() {
+  if (c.lock) {
+    c.x = mouseX - 10;
+    c.y = mouseY - 10;
   }
 }
-function mousePressed(){
-  if(c.over){
-    c.lock = true
+function mousePressed() {
+  if (c.over && revenue  > 0) {
+    c.lock = true;
   }
   
 }
-function cannonStuff(){
-     c.display() 
-    c.grab()
+function cannonStuff() {
+  c.display(); 
+  c.grab();
 }
-function enemyStuff(){
-   e.display()
-  e.followPath()
+function enemyStuff() {
+  e.display();
+  e.followPath();
 }
