5e25995fa61da2ec2eeed7e96a975db753ee3563
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-03 13:41:25 -0500

    Update cannon.js

--- cannon.js
+++ cannon.js
@@ -6,10 +6,25 @@ function Cannon(x,y,w,h,img){
   this.img = loadImage(img)
   this.over = false;
   this.lock = false
+  this.xPlaced = x;
+  this.yPlaced = y;
+  this.isPlaced = false;
+  this.angle = 45;
   
   this.display = function(){
     this.img.resize(this.w, this.h);
-    image(this.img, this.x, this.y)
+    if(this.isPlaced){
+      push()
+      translate(this.xPlaced, this.yPlaced);
+      angleMode(DEGREES);
+      rotate(this.angle)
+      image(this.img, -15,-15);
+      pop()
+      this.angle += 1
+    }else{
+      image(this.img, this.x, this.y)
+    }
+    
   }
   this.grab = function(){
     if(mouseX > this.x &&
