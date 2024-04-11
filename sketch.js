8eb07c4886adc5498e6470dadcd123c3c56bc4a9
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-11 13:34:08 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,15 +5,17 @@ let x;
 let sendButton;
 let revenue;
 let myFont;
+let waveCount;
+let stopSending;
 
 function setup() {
   createCanvas(500, 400);
   img = loadImage("towerGood.png");
   c = new Cannon(410, 50, 30, 30, "cannon.png");
   revenue = 100;
-  myFont = loadFont("Anta.ttf")
+  myFont = loadFont("Anta.ttf");
   sendButton = createButton("send em");
-  
+  waveCount = 10;
 }
 function draw() {
   background(220);
@@ -24,33 +26,36 @@ function draw() {
   textStuff();
   sendButtonStuff();
 }
-
-function sendButtonStuff(){
-  sendButton.position(410, 150)
-  sendButton.style("background-color", "red")
-  sendButton.mousePressed(()=>{
+function sendButtonStuff() {
+  sendButton.position(410, 150);
+  sendButton.style("background-color", "red");
+  sendButton.mousePressed(() => {
     sendButton.hide();
-    setInterval(createEnemy,1000);
-  })
+    stopSending = setInterval(createEnemy, 1000);
+  });
 }
-function createEnemy(){
- 
-  e.push(new Enemy(0,159,20,20,1))
+function createEnemy() {
+  e.push(new Enemy(0, 159, 20, 20, 1));
+  waveCount -= 1;
+  if(waveCount < 1){
+    clearInterval(stopSending);
+    waveCount = 10
+    sendButton.show();
+  }
 }
-function textStuff(){
-  fill("black")
-  textSize(10)
-  textFont(myFont)
-  text("revenue: " + revenue, 410,10)
-  text("cost: 1 ", 410,100)
+function textStuff() {
+  fill("black");
+  textSize(10);
+  textFont(myFont);
+  text("revenue: " + revenue, 410, 10);
+  text("cost: 1 ", 410, 100);
 }
 function mouseReleased() {
   c.lock = false;
-  revenue -= 1
+  revenue -= 1;
   c.xPlaced = c.x;
   c.yPlaced = c.y;
   c.isPlaced = true;
-  
 }
 function mouseDragged() {
   if (c.lock) {
@@ -59,19 +64,17 @@ function mouseDragged() {
   }
 }
 function mousePressed() {
-  if (c.over && revenue  > 0) {
+  if (c.over && revenue > 0) {
     c.lock = true;
   }
-  
 }
 function cannonStuff() {
-   c.display()
+  c.display();
   c.grab();
 }
 function enemyStuff() {
-  for(i=0 ; i<e.length ; i++){
+  for (i = 0; i < e.length; i++) {
     e[i].display();
     e[i].followPath();
   }
-  
 }
