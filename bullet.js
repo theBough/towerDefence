debe21dfa43b7184e8f7ebd1392a7b0e64c77b46
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-16 13:01:41 -0500

    Update bullet.js

--- bullet.js
+++ bullet.js
@@ -1,14 +1,27 @@
-function Bullet(x,y,w,h,xSpeed, ySpeed){
+function Bullet(x, y, w, h, xSpeed, ySpeed) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.xSpeed = xSpeed;
   this.ySpeed = ySpeed;
-  
-  this.display = function(){
-    rect(this.x, this.y, this.w, this.h)
+
+  this.display = function () {
+    rect(this.x, this.y, this.w, this.h);
     this.x += this.xSpeed;
-    this.y += this.ySpeed
-  }//end Display
-}//end Function
+    this.y += this.ySpeed;
+  }; //end Display
+  this.collision = function () {
+    for(var i =0 ; i< e.length ; i++){
+      if (
+      e[i].x > this.x &&
+      e[i].x < this.x + this.w &&
+      e[i].y > this.y &&
+      e[i].y < this.y + this.h
+    ) {
+      e.splice(i,1);
+      }//end if
+    }//end loop
+    
+  };
+} //end Function
